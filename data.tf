8105cda13a64443f773c4d1d55e041d0702a6d93
Richard Mah <dick.mah@gmail.com>
2020-03-25 15:05:28 -0400

    ASG will now properly wait on packer and use generated image Uses image family of "polkadot-sentry"

--- data.tf
+++ data.tf
@@ -1,6 +1,7 @@
 data "google_client_config" "this" {}
 
 data "google_compute_image" "ubuntu" {
-  family  = "ubuntu-minimal-1804-lts"
-  project = "ubuntu-os-cloud"
+  family     = "polkadot-sentry"
+  project    = var.project
+  depends_on = [module.packer]
 }
