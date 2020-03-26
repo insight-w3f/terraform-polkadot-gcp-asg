d8fca83a379b8c081ff3e13292e666fea7e43423
Richard Mah <dick.mah@gmail.com>
2020-03-26 19:17:49 -0400

    Update CCI config and defaults test

--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -1,7 +1,8 @@
 provider "google" {}
 
 module "network" {
-  source = "github.com/insight-infrastructure/terraform-polkadot-gcp-network.git?ref=master"
+  source   = "github.com/insight-infrastructure/terraform-polkadot-gcp-network.git?ref=master"
+  vpc_name = "cci-test"
 }
 
 module "defaults" {
@@ -10,7 +11,8 @@ module "defaults" {
   relay_node_ip          = "1.2.3.4"
   relay_node_p2p_address = "abcdefg"
   security_group_id      = module.network.sentry_security_group_id[0]
-  subnet_id              = module.network.public_subnets[0]
   vpc_id                 = module.network.vpc_id
   network_name           = "dev"
+  private_subnet_id      = module.network.private_subnets[0]
+  public_subnet_id       = module.network.public_subnets[0]
 }
